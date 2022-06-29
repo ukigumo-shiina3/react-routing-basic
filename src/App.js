04112984b0a465a808909a490f5105d00c2b8264
daisuke33 <67767613+ukigumo-shiina3@users.noreply.github.com>
2022-06-29 14:03:24 +0900

    ✨ ネストされたページ遷移

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./styles.css";
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
 import { Page2 } from "./Page2";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 
 export default function App() {
   return (
@@ -18,9 +20,23 @@ export default function App() {
         <Route exact path="/">
           <Home />
         </Route>
-        <Route exact path="/page1">
-          <Page1 />
-        </Route>
+        <Route
+          path="/page1"
+          render={({ match: { url } }) => (
+            <Switch>
+              {console.log(url)}
+              <Route exact path={url}>
+                <Page1 />
+              </Route>
+              <Route path={`${url}/detailA`}>
+                <Page1DetailA />
+              </Route>
+              <Route path={`${url}/detailA`}>
+                <Page1DetailB />
+              </Route>
+            </Switch>
+          )}
+        ></Route>
         <Route exact path="/page2">
           <Page2 />
         </Route>

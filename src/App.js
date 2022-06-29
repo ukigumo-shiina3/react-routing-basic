71eaa0b2f9ba6c1cca76a0d214516832d3981bd1
daisuke33 <67767613+ukigumo-shiina3@users.noreply.github.com>
2022-06-29 10:57:38 +0900

    ✨ 基本的なページ遷移

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 import "./styles.css";
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/Page1">Page1</Link>
+        <br />
+        <Link to="/Page2">Page2</Link>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/page1">
+          <Page1 />
+        </Route>
+        <Route exact path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }

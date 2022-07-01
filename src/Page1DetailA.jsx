80eaeb250b0335eb47ede2db561d46b00a3cc130
daisuke33 <67767613+ukigumo-shiina3@users.noreply.github.com>
2022-07-01 10:15:59 +0900

    ✨ URLパラメーター

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const { id } = useParams();
   return (
     <div>
       <h1>Page1DetailAページです</h1>
+      <p>パラメーターは{id}です</p>
     </div>
   );
 };
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,7 +1,7 @@
 import { Route, Switch } from "react-router-dom";
 import { Home } from "../Home";
-import { Page2 } from "../Page2";
 import { Page1Routes } from "./Page1Routes";
+import { Page2Routes } from "./Page2Routes";
 
 export const Router = () => {
   return (
@@ -25,9 +25,22 @@ export const Router = () => {
           </Switch>
         )}
       ></Route>
-      <Route exact path="/page2">
-        <Page2 />
-      </Route>
+      <Route
+        path="/page2"
+        render={({ match: { url } }) => (
+          <Switch>
+            {Page2Routes.map((route) => (
+              <Route
+                key={route.parh}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      ></Route>
     </Switch>
   );
 };
